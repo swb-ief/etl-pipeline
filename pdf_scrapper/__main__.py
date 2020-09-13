de9a288dc65f29a9438889dce9d114fe40f6bc9c
rscnt <rascencio@gmail.com>
2020-09-13 01:35:38 -0600

    feat: pushing github workflows to fetch files.
    Signed-off-by: rscnt <rascencio@gmail.com>

--- pdf_scrapper/__main__.py
+++ pdf_scrapper/__main__.py
@@ -1,34 +1,53 @@
 import os
+from urllib.request import urlretrieve
 
 import click
 
 from .stop_covid_dashboard_scrapper import scrap_pdf_to_csv
 
+@click.command()
+@click.option("--dashboard-pdf-url", default="http://stopcoronavirus.mcgm.gov.in/assets/docs/Dashboard.pdf", show_default=True)
+@click.option("--output", default="stopcovid-dashboard.pdf", show_default=True)
+def download_pdf(dashboard_pdf_url, output):
+    click.echo(f"Fetching {dashboard_pdf_url} into {output}")
+    return urlretrieve(dashboard_pdf_url, output)
+
 
 @click.command()
 @click.argument("pdf-files", nargs=-1, required=True)
 @click.argument("output-dir", nargs=1, required=True)
-def scrap_pdf(pdf_files, output_dir):
+@click.option(
+    "--page-with-positive-breakdown",
+    default=22,
+    help="The page containing the ward-wise breakdown of positive cases.",
+    show_default=True
+)
+def scrap_pdf(pdf_files, output_dir, page_with_positive_breakdown):
     """Given a PDF_FILE tries to fetch the data from all wards.
 
     We assume the PDF_FILE has been downloaded from http://stopcoronavirus.mcgm.gov.in/assets/docs/Dashboard.pdf.
 
-    Args:
-        pdf_files ([string or file handle]): The path to the pdf file or a buffer of the file.
-        output_dir ([string]): The path to the output directory.
+    PDF_FILES A list of paths to the pdf files.
+    OUTPUT_DIR The path to the output directory.
     """
     if not os.path.isdir(output_dir):
         raise IOError(f"Output: {output_dir} is not a directory")
 
     paths = []
     for pdf_file in pdf_files:
-        date, csv_path = scrap_pdf_to_csv(pdf_file, output_dir)
+        date, csv_path = scrap_pdf_to_csv(pdf_file, output_dir, page_with_positive_breakdown)
         paths.append(csv_path)
         click.echo(f"Data from {date} has been downloaded to {csv_path}")
 
     click.echo(f"Saved the following files {paths}")
     return paths
 
+@click.group()
+def cli():
+    pass
+
+cli.add_command(download_pdf)
+cli.add_command(scrap_pdf)
 
 if __name__ == "__main__":
-    scrap_pdf()
+    cli()
