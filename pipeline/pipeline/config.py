86aae0e700cc4b9fea65a8c647a8994845dbd78e
rscnt <rascnt@gmail.com>
2020-10-05 01:59:58 -0600

    chore: adding warning for gspread client
    Signed-off-by: rscnt <rascnt@gmail.com>

--- pipeline/pipeline/config.py
+++ pipeline/pipeline/config.py
@@ -1,4 +1,5 @@
 import os
+import logging
 
 import gspread
 
@@ -7,9 +8,13 @@ from luigi.contrib.dropbox import DropboxClient
 DROPBOX_TOKEN = os.getenv("SWB_DROPBOX_TOKEN")
 dropbox_client = DropboxClient(DROPBOX_TOKEN)
 
-GSPREAD_CLIENT = gspread.service_account()
+GSPREAD_CLIENT = None
+DEFAULT_WORKSHEET_URL = "https://docs.google.com/spreadsheets/d/1HeTZKEXtSYFDNKmVEcRmF573k2ZraDb6DzgCOSXI0f0/edit#gid=0"
 
-WORKSHEET_URL = os.getenv(
-    "SWB_WORKSHEET_URL",
-    "https://docs.google.com/spreadsheets/d/1HeTZKEXtSYFDNKmVEcRmF573k2ZraDb6DzgCOSXI0f0/edit#gid=0",
-)
+
+WORKSHEET_URL = os.getenv("SWB_WORKSHEET_URL", DEFAULT_WORKSHEET_URL)
+
+try:
+    GSPREAD_CLIENT = gspread.service_account()
+except Exception as e:
+    logging.error(f"Unable to create gspread client #{e.message}")
