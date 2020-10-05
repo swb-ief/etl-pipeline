d35abc1a4ae972e0db88059701f7cc244808484e
rscnt <rascnt@gmail.com>
2020-10-05 01:40:08 -0600

    chore: formatting
    Signed-off-by: rscnt <rascnt@gmail.com>

--- pipeline/pipeline/config.py
+++ pipeline/pipeline/config.py
@@ -9,6 +9,7 @@ dropbox_client = DropboxClient(DROPBOX_TOKEN)
 
 GSPREAD_CLIENT = gspread.service_account()
 
-WORKSHEET_URL = (
-    os.getenv("SWB_WORKSHEET_URL", "https://docs.google.com/spreadsheets/d/1HeTZKEXtSYFDNKmVEcRmF573k2ZraDb6DzgCOSXI0f0/edit#gid=0")
+WORKSHEET_URL = os.getenv(
+    "SWB_WORKSHEET_URL",
+    "https://docs.google.com/spreadsheets/d/1HeTZKEXtSYFDNKmVEcRmF573k2ZraDb6DzgCOSXI0f0/edit#gid=0",
 )
--- pipeline/pipeline/scripts/scrap_old_pdfs_to_gsheet.py
+++ pipeline/pipeline/scripts/scrap_old_pdfs_to_gsheet.py
@@ -18,25 +18,36 @@ import luigi
 from pipeline.tasks.spreadsheets import ExtractDataFromPdfDashboardGSheetWrapper
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Reads the previous PDFs with pages CSV URL.')
-    parser.add_argument('csv_uri', type=str, help='The URL or Path to the CSV with the argument list for ExtractDataFromPdfDashboardGSheetWrapper')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Reads the previous PDFs with pages CSV URL."
+    )
+    parser.add_argument(
+        "csv_uri",
+        type=str,
+        help="The URL or Path to the CSV with the argument list for ExtractDataFromPdfDashboardGSheetWrapper",
+    )
     csv_uri = parser.parse_args().csv_uri
     print(f"CSV URI {csv_uri}")
     # pandas#read_csv manages the download and parsing of values.
     task_args_df = pandas.read_csv(csv_uri)
-    task_args_df['date-of-pdf'] = pandas.to_datetime(task_args_df['date-of-pdf'])
+    task_args_df["date-of-pdf"] = pandas.to_datetime(task_args_df["date-of-pdf"])
     # the positive breakdown page task uses the page index instead of the page number.
-    task_args_df['positive-breakdown-page'] = task_args_df['positive-breakdown-page'] - 1
+    task_args_df["positive-breakdown-page"] = (
+        task_args_df["positive-breakdown-page"] - 1
+    )
     task_list = []
     for index, row in task_args_df.iterrows():
         task_list.append(
             ExtractDataFromPdfDashboardGSheetWrapper(
-                date=row['date-of-pdf'], 
-                elderly_page=row['elderly-table-page'], 
-                daily_case_growth_page=row['case-growth-page'],
-                positive_breakdown_index=row['positive-breakdown-page']
-                ))
+                date=row["date-of-pdf"],
+                elderly_page=row["elderly-table-page"],
+                daily_case_growth_page=row["case-growth-page"],
+                positive_breakdown_index=row["positive-breakdown-page"],
+            )
+        )
     # We assume we don't have a luigi deamon running.
-    luigi_run_result = luigi.build(task_list, local_scheduler=True, detailed_summary=True)
+    luigi_run_result = luigi.build(
+        task_list, local_scheduler=True, detailed_summary=True
+    )
     print(luigi_run_result.summary_text)
