d96011303f022a1fd9332aabf0cc15d51b924c6d
Krishna Teja Rekapalli <krrish1729@gmail.com>
2020-11-03 11:58:17 -0600

    Update push_Rt_to_gsheets.py
    fix indentation issues

--- pipeline/pipeline/tasks/push_Rt_to_gsheets.py
+++ pipeline/pipeline/tasks/push_Rt_to_gsheets.py
@@ -10,10 +10,10 @@ def worksheet_as_df_by_url(sheet_url: str, worksheet_name: str) -> Tuple[gspread
     worksheet = sheet.worksheet(worksheet_name)
     return worksheet, pandas.DataFrame(worksheet.get_all_records())
     
- worksheet, Rt_df = worksheet_as_df_by_url(WORKSHEET_URL, "Rt")
+worksheet, Rt_df = worksheet_as_df_by_url(WORKSHEET_URL, "Rt")
  
- new_Rt_df = pd.read_csv("/usr/data/epinow2_out.csv")
+new_Rt_df = pd.read_csv("/usr/data/epinow2_out.csv")
  
- worksheet.update([new_Rt_df.columns.values.tolist()] + new_Rt_df.values.tolist())
+worksheet.update([new_Rt_df.columns.values.tolist()] + new_Rt_df.values.tolist())
  
  
