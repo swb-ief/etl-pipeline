43ab84943641f3080b553b2341865de0ba44f1c3
Nozziel <nozziel.u.shadow@gmail.com>
2020-12-12 18:11:02 +0100

    Update dropbox_clean.py
    moved some more code in here.. .for testing.

--- pipeline/pipeline/tasks/dropbox_clean.py
+++ pipeline/pipeline/tasks/dropbox_clean.py
@@ -1,13 +1,21 @@
 # from pipeline.config import DROPBOX_TOKEN
-import dropbox
+from luigi.contrib.dropbox import DropboxTarget
+from pipeline.config import DROPBOX_TOKEN
 import os
+
+DROPBOX_TOKEN = os.getenv("SWB_DROPBOX_TOKEN") # TODO get from pipeline.config
+
+# duplicate of a func in dropbox.py
+def dropbox_target(path):
+    return DropboxTarget(path, DROPBOX_TOKEN) 
+
 
 # ? ==============================
 # ? dropbox_clear_space
 # ? Additions re: Dropbox clear space
 def ensure_available_space(min_space):
 
-    dbx = dropbox.Dropbox(os.getenv("SWB_DROPBOX_TOKEN")) # TODO get from pipeline.config
+    dbx = dropbox.Dropbox(DROPBOX_TOKEN) 
 
     # ? print usage
     usage = dbx.users.SpaceUsage()
