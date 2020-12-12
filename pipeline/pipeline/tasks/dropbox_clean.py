e8277fbc41b06f11dbdbd52224d1bee515b86df0
Miguel Rivera-Lanas <mriveralanas@pop-os.localdomain>
2020-12-12 00:17:44 -0500

    ran linter

--- pipeline/pipeline/tasks/dropbox_clean.py
+++ pipeline/pipeline/tasks/dropbox_clean.py
@@ -1,23 +1,29 @@
-
 from pipeline.config import DROPBOX_TOKEN
-import dropbox 
+import dropbox
 
-#? ==============================
-#? dropbox_clear_space
-#? Additions re: Dropbox clear space
-def available_space():
+# ? ==============================
+# ? dropbox_clear_space
+# ? Additions re: Dropbox clear space
+def ensure_available_space(min_space):
 
     dbx = dropbox.Dropbox(DROPBOX_TOKEN)
 
-    #? list files
-    for entry in dbx.files_list_folder('').entries:
+    # ? print usage
+    usage = dbx.users.SpaceUsage()
+    print(usage)
+
+    # ? list files
+    for entry in dbx.files_list_folder("").entries:
         print(entry.name)
         print(dbx.files_get_metadata(entry.name))
 
+    # ? delete file
+    # path = ""
+    # dbx.files_delete(path)
 
     return None
 
 
 if __name__ == "__main__":
 
-    available_space()
+    ensure_available_space()
