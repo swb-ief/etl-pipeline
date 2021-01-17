dca6336f6f831b147e216b2f59787ad440b5a757
Nozziel <nozziel.u.shadow@gmail.com>
2021-01-17 16:17:48 +0100

    switched to csv

--- backend/data/static_ward_data.py
+++ backend/data/static_ward_data.py
@@ -1,9 +1,9 @@
 import os
 import pandas as pd
-import json
 
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 def get_static_ward_data() -> pd.DataFrame:
-    pass
+    df = pd.read_csv(os.path.join(THIS_DIR, 'static_ward_data.csv'))
+    return df
---
+++ tests/test_data.py
@@ -0,0 +1,8 @@
+from backend.data import get_static_ward_data
+
+
+def test_get_static_ward_data():
+    expected = 'population'
+    result = get_static_ward_data()
+
+    assert expected in result.columns
