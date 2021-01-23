91156c8feb3b6b20af53f1b7b21e24a2d6cc1157
Miguel Rivera-Lanas <mriveralanas@pop-os.localdomain>
2021-01-23 17:56:51 -0500

    test RT df for new issue

--- R_scripts/Rt_calcs.R
+++ R_scripts/Rt_calcs.R
@@ -91,19 +91,29 @@ reporting_delay$max <- 30
 # values for generation time and incubation period have been defined now.
 # the code below is for v 1.3.0 package.
 # set credible interval as 0.95
-rt <-
-  EpiNow2::epinow(
-    reported_cases = df3,
-    generation_time = generation_time,
-    delays = delay_opts(incubation_period, reporting_delay),
-    rt = rt_opts(prior = list(mean = 2, sd = 0.2)),
-    stan = stan_opts(cores = 4, samples = 100),
-    verbose = TRUE,
-    CrIs = 0.95
-  )
+# rt <-
+#   EpiNow2::epinow(
+#     reported_cases = df3,
+#     generation_time = generation_time,
+#     delays = delay_opts(incubation_period, reporting_delay),
+#     rt = rt_opts(prior = list(mean = 2, sd = 0.2)),
+#     stan = stan_opts(cores = 4, samples = 100),
+#     verbose = TRUE,
+#     CrIs = 0.95
+#   )
 
-# get the summary estimates with the credible intervals.
-rt <- summary(rt, type = "parameters", params = "R")
+# # get the summary estimates with the credible intervals.
+# rt <- summary(rt, type = "parameters", params = "R")
+
+# test RT 
+date <- c('2021-01-15', '2021-01-16','2021-01-17','2021-01-18','2021-01-19')
+variable <- c('R','R','R','R','R')
+mean <- c(.968,.968,.968,.968,.968)
+sd <- c(.131,.131,.131,.131,.131 )
+lower_95 <- c(.76,.76,.76,.76,.76)
+upper_95 <- c(1.14,1.14,1.14,1.14,1.14)
+rt <- data.frame(date, variable, strat, type, median, mean, sd, lower_95, upper_95)
+
 
 # ============================
 # Doubling time
