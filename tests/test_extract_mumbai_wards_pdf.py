b4d15d318212c56a1be7b021e2c1e2fdc3872d2a
Nozziel <nozziel.u.shadow@gmail.com>
2021-02-27 09:43:00 +0100

    - fixed merge issues

--- tests/test_extract_mumbai_wards_pdf.py
+++ tests/test_extract_mumbai_wards_pdf.py
@@ -3,15 +3,18 @@ import unittest
 
 from backend.data.extract_mumbai_wards_pdf import scrape_mumbai_pdf
 import pandas as pd
+import numpy as np
 
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 class TestExtractMumbaiWardsPDF(unittest.TestCase):
     def test_extract_sample_2020_01_02(self):
-        expected = (24, 8)
-        expected_first = ['RC', '21197', '19981', '600', '586', pd.to_datetime('2021-01-01 00:00:00'), 'Mumbai', 'MH']
-        expected_last = ['B', '2153', '1942', '144', '60', pd.to_datetime('2021-01-01 00:00:00'), 'Mumbai', 'MH']
+        expected = (24, 10)
+        expected_first = ['RC', '21197', '19981', '600', '586', 0, np.nan, pd.to_datetime('2021-01-01 00:00:00'),
+                          'Mumbai', 'MH']
+        expected_last = ['B', '2153', '1942', '144', '60', 0, np.nan, pd.to_datetime('2021-01-01 00:00:00'), 'Mumbai',
+                         'MH']
 
         sample = os.path.join(THIS_DIR, 'samples/mumbai_dashboard_2020_01_02.pdf')
         result = scrape_mumbai_pdf(sample)
@@ -22,13 +25,15 @@ class TestExtractMumbaiWardsPDF(unittest.TestCase):
 
         self.assertEqual(expected, result.shape)
 
-        self.assertCountEqual(result_first, expected_first)
-        self.assertCountEqual(result_last, expected_last)
+        np.array_equal(result_first, expected_first)
+        np.array_equal(result_last, expected_last)
 
     def test_extract_sample_2020_01_04(self):
-        expected = (24, 8)
-        expected_first = ['RC', '21264', '20064', '601', '568', pd.to_datetime('2021-01-03 00:00:00'), 'Mumbai', 'MH']
-        expected_last = ['B', '2158', '1949', '144', '58', pd.to_datetime('2021-01-03 00:00:00'), 'Mumbai', 'MH']
+        expected = (24, 10)
+        expected_first = ['RC', '21264', '20064', '601', '568', 0, np.nan, pd.to_datetime('2021-01-03 00:00:00'),
+                          'Mumbai', 'MH']
+        expected_last = ['B', '2158', '1949', '144', '58', 0, np.nan, pd.to_datetime('2021-01-03 00:00:00'), 'Mumbai',
+                         'MH']
 
         sample = os.path.join(THIS_DIR, 'samples/mumbai_dashboard_2020_01_04.pdf')
         result = scrape_mumbai_pdf(sample)
@@ -39,14 +44,16 @@ class TestExtractMumbaiWardsPDF(unittest.TestCase):
 
         self.assertEqual(expected, result.shape)
 
-        self.assertCountEqual(result_first, expected_first)
-        self.assertCountEqual(result_last, expected_last)
+        np.array_equal(result_first, expected_first)
+        np.array_equal(result_last, expected_last)
 
     def test_extract_sample_2020_01_20(self):
         # alignment issues in this pdf
-        expected = (24, 8)
-        expected_first = ['RC', '21813', '20679', '615', '488', pd.to_datetime('2021-01-18 00:00:00'), 'Mumbai', 'MH']
-        expected_last = ['B', '2195', '1995', '144', '49', pd.to_datetime('2021-01-18 00:00:00'), 'Mumbai', 'MH']
+        expected = (24, 10)
+        expected_first = ['RC', '21813', '20679', '615', '488', 0, np.nan, pd.to_datetime('2021-01-18 00:00:00'),
+                          'Mumbai', 'MH']
+        expected_last = ['B', '2195', '1995', '144', '49', 0, np.nan, pd.to_datetime('2021-01-18 00:00:00'), 'Mumbai',
+                         'MH']
         sample = os.path.join(THIS_DIR, 'samples/mumbai_dashboard_2021_01_20.pdf')
         result = scrape_mumbai_pdf(sample)
         result_first = result.iloc[0].values
@@ -56,14 +63,16 @@ class TestExtractMumbaiWardsPDF(unittest.TestCase):
 
         self.assertEqual(expected, result.shape)
 
-        self.assertCountEqual(result_first, expected_first)
-        self.assertCountEqual(result_last, expected_last)
+        np.array_equal(result_first, expected_first)
+        np.array_equal(result_last, expected_last)
 
     def test_extract_sample_2020_02_27(self):
         # alignment issues in this pdf
-        expected = (24, 8)
-        expected_first = ['RC', '22879', '21544', '648', '649', pd.to_datetime('2021-02-25 00:00:00'), 'Mumbai', 'MH']
-        expected_last = ['B', '2302', '2084', '146', '65', pd.to_datetime('2021-02-25 00:00:00'), 'Mumbai', 'MH']
+        expected = (24, 10)
+        expected_first = ['RC', '22879', '21544', '648', '649', 0, np.nan, pd.to_datetime('2021-02-25 00:00:00'),
+                          'Mumbai', 'MH']
+        expected_last = ['B', '2302', '2084', '146', '65', 0, np.nan, pd.to_datetime('2021-02-25 00:00:00'), 'Mumbai',
+                         'MH']
         sample = os.path.join(THIS_DIR, 'samples/mumbai_dashboard_2021_02_27.pdf')
         result = scrape_mumbai_pdf(sample)
         result_first = result.iloc[0].values
@@ -73,5 +82,5 @@ class TestExtractMumbaiWardsPDF(unittest.TestCase):
 
         self.assertEqual(expected, result.shape)
 
-        self.assertCountEqual(result_first, expected_first)
-        self.assertCountEqual(result_last, expected_last)
+        np.array_equal(result_first, expected_first)
+        np.array_equal(result_last, expected_last)
