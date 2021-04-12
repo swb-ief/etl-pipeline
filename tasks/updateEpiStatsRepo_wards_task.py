0400bb0dca07860c3b08cd52c30a659146679697
aswinjayan94 <aswinjayan94@gmail.com>
2021-04-12 08:15:06 +0530

    Added DT

--- tasks/updateEpiStatsRepo_wards_task.py
+++ tasks/updateEpiStatsRepo_wards_task.py
@@ -20,7 +20,7 @@ class UpdateEpiStatsWardsTask(luigi.Task):
     s3_rt_path = 'Phase2_RT_Wards'
     s3_dt_path = 'Phase2_DT_Wards'
     s3_wards_path = 'Phase 2 - Wards'
-    s3_wards_rt_path = 'Phase 2 - Wards - With RT'
+    s3_wards_rt_path = 'Phase 2 - Wards_RT_DT'
 
     def requires(self):
         return CalcRTWardTask(file_name = self.local_rt_path)#, CalcDTTask(file_name = self.local_dt_path)
@@ -29,6 +29,13 @@ class UpdateEpiStatsWardsTask(luigi.Task):
         config = get_config()
         repository = AWSFileRepository(config['aws']['bucket production'])
 
+        # import master districts data
+        all_wards = repository.get_dataframe(self.s3_wards_path)
+        del all_wards['mean.RT']
+        del all_wards['upper.RT']
+        del all_wards['lower.RT']
+        del all_wards['dt']
+        
         # read RT results
         rt_results0 = pd.read_csv(self.local_rt_path, parse_dates=["date"])
         
@@ -36,24 +43,17 @@ class UpdateEpiStatsWardsTask(luigi.Task):
         rt_colname = ['mean', 'upper', 'lower']
         rt_results = rt_results0[['ward', 'date']+rt_colname]
         rt_results.columns = ['ward', 'date', 'mean.RT', 'upper.RT', 'lower.RT']
-        
-        # import master districts data
-        all_wards = repository.get_dataframe(self.s3_wards_path)
-        
+
         # join rt data with all districts data
         all_wards = all_wards.merge(rt_results, on=['ward', 'date'], how='left')
+    
+        # read DT results
+        dt_results0 = pd.read_csv(self.local_dt_path, parse_dates=["date"])
+        # pick only the relevant columns
+        dt_results = dt_results0[["ward", "date", "dt"]]
+        all_wards = all_wards.merge(dt_results, on=['ward', 'date'], how='left')
     
         # push RT to Repo
         repository.store_dataframe(all_wards, self.s3_wards_rt_path, allow_create=True)
 
-        # # read DT results
-        # dt_results = pd.read_csv(self.local_dt_path)
-        
-        # # push DT to Repo
-        # repository.store_dataframe(dt_results, self.s3_dt_path, allow_create=True)
-
-
-    # def complete(self):
-    #     # TODO --> palceholder for some kind of validation process,
-    #     # TODO, similar to https://github.com/swb-ief/etl-pipeline/blob/master/tasks/update_dashboard_task.py
-    #     return True
+       
