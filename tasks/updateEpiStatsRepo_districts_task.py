2a016f88103c7e79050e224a901f5d56d65d5372
aswinjayan94 <aswinjayan94@gmail.com>
2021-04-12 19:53:10 +0530

    Create tasks for RT/DT calculation for wards/districts

---
+++ tasks/updateEpiStatsRepo_districts_task.py
@@ -0,0 +1,64 @@
+import logging
+from datetime import date
+import subprocess
+import pandas as pd
+
+import luigi
+from backend.config import get_config
+from backend.repository import AWSFileRepository
+from tasks.epi_stats.calcRT_task import CalcDistrictsRTTask
+from tasks.epi_stats.calcDT_task import CalcDTTask
+
+log = logging.getLogger(__name__)
+
+class UpdateEpiStatsDistrictsTask(luigi.Task):
+
+    # local file paths
+    local_rt_path = "/usr/data/epiestim_out_districts.csv"
+    local_dt_path = "/usr/data/doubling_time_districts.csv"
+    citystats_path = '/usr/data/citystats.csv' 
+
+
+    # aws s3 paths
+    s3_rt_path = 'Phase2_RT'
+    s3_dt_path = 'Phase2_DT'
+    s3_districts_path = 'Phase 2 - Districts'
+    s3_districts_update_path = 'Phase 2 - Districts'
+
+    def requires(self):
+        return CalcDTTask(file_name = self.local_dt_path), CalcDistrictsRTTask(file_name = self.local_rt_path)
+
+    def run(self):
+        config = get_config()
+        repository = AWSFileRepository(config['aws']['bucket production'])
+
+        # import master districts data
+        all_districts = repository.get_dataframe(self.s3_districts_path)
+        
+        # delete existing RT/DT columns
+        
+        avl_cols = [x for x in all_districts.columns if x in ['mean.RT', 'upper.RT', 'lower.RT', 'dt']]
+        for avl_col in avl_cols:
+            del all_districts[avl_col]
+
+        # read RT results
+        rt_results0 = pd.read_csv(self.local_rt_path, parse_dates=["date"])
+        # specify column name containing Rt values
+        rt_colname = ['mean', 'upper', 'lower']
+        rt_results = rt_results0[['city', 'date']+rt_colname]
+        rt_results.columns = ['district', 'date', 'mean.RT', 'upper.RT', 'lower.RT']
+        # join rt data with all districts data
+        all_districts = all_districts.merge(rt_results, on=['district', 'date'], how='left')
+    
+        # read DT results
+        dt_results0 = pd.read_csv(self.local_dt_path, parse_dates=["date"])
+        # pick only the relevant columns
+        dt_results = dt_results0[["district", "date", "dt"]]
+        all_districts = all_districts.merge(dt_results, on=['district', 'date'], how='left')
+
+        # push RT/DT Critical Cities Updates to Repo
+        repository.store_dataframe(all_districts, self.s3_districts_update_path, allow_create=True)
+
+
+
+
---
+++ tasks/updateEpiStatsRepo_wards_task.py
@@ -0,0 +1,58 @@
+import logging
+from datetime import date
+import subprocess
+import pandas as pd
+
+import luigi
+from backend.config import get_config
+from backend.repository import AWSFileRepository
+from tasks.epi_stats.calcRT_ward_task import CalcRTWardTask
+from tasks.epi_stats.calcDT_ward_task import CalcDTWardTask
+
+log = logging.getLogger(__name__)
+
+class UpdateEpiStatsWardsTask(luigi.Task):
+
+    # local file paths
+    local_rt_path = "/usr/data/epiestim_out_wards.csv"
+    local_dt_path = "/usr/data/doubling_time_wards.csv"
+    # aws s3 paths
+    s3_rt_path = 'Phase2_RT_Wards'
+    s3_dt_path = 'Phase2_DT_Wards'
+    s3_wards_path = 'Phase 2 - Wards'
+    s3_wards_update_path = 'Phase 2 - Wards'
+
+    def requires(self):
+        return  CalcDTWardTask(file_name = self.local_dt_path), CalcRTWardTask(file_name = self.local_rt_path)
+
+    def run(self):
+        config = get_config()
+        repository = AWSFileRepository(config['aws']['bucket production'])
+
+        # import master districts data
+        all_wards = repository.get_dataframe(self.s3_wards_path)
+        avl_cols = [x for x in all_wards.columns if x in ['mean.RT', 'upper.RT', 'lower.RT', 'dt']]
+        for avl_col in avl_cols:
+            del all_wards[avl_col]
+        
+        # read RT results
+        rt_results0 = pd.read_csv(self.local_rt_path, parse_dates=["date"])
+        
+        # specify column name containing Rt values
+        rt_colname = ['mean', 'upper', 'lower']
+        rt_results = rt_results0[['ward', 'date']+rt_colname]
+        rt_results.columns = ['ward', 'date', 'mean.RT', 'upper.RT', 'lower.RT']
+
+        # join rt data with all districts data
+        all_wards = all_wards.merge(rt_results, on=['ward', 'date'], how='left')
+    
+        # read DT results
+        dt_results0 = pd.read_csv(self.local_dt_path, parse_dates=["date"])
+        # pick only the relevant columns
+        dt_results = dt_results0[["ward", "date", "dt"]]
+        all_wards = all_wards.merge(dt_results, on=['ward', 'date'], how='left')
+    
+        # push RT to Repo
+        repository.store_dataframe(all_wards, self.s3_wards_update_path, allow_create=True)
+
+       
