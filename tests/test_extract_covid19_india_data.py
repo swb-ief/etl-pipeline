aa85e21ca41eb0c60a1d618f5cf38d5146cbee33
aswinjayan94 <aswinjayan94@gmail.com>
2021-08-19 19:19:17 +0530

    Update unit test for ExtractCovid19IndiaData

--- tests/test_extract_covid19_india_data.py
+++ tests/test_extract_covid19_india_data.py
@@ -1,6 +1,7 @@
 import os
 import unittest
 import json
+import pandas as pd
 
 from backend.data import ExtractCovid19IndiaData
 
@@ -10,13 +11,14 @@ THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 class TestExtractCovid19IndiaData(unittest.TestCase):
 
     def test_load_sample(self):
-        with open(os.path.join(THIS_DIR, 'samples/covid19india_data-all.json')) as file:
-            covid_data = json.load(file)
+        
+        states_rawdata = pd.read_csv(os.path.join(THIS_DIR, 'samples/states_data_sample.csv'), parse_dates=["date"])
+        districts_rawdata = pd.read_csv(os.path.join(THIS_DIR, 'samples/districts_data_sample.csv'), parse_dates=["date"])
 
         sut = ExtractCovid19IndiaData()
-        df_state, df_district = sut.process(covid_data)
+        df_state, df_district = sut.process(states_rawdata, districts_rawdata)
 
-        expected_state = (10347, 14)
-        expected_district = (156_792, 15)
+        expected_state = (1000, 14)
+        expected_district = (10000, 15)
         self.assertEqual(expected_state, df_state.shape)
         self.assertEqual(expected_district, df_district.shape)
