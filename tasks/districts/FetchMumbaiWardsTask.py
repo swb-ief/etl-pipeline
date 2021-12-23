2aef0480ec2cef70942234753b563e880595433d
aswinjayan94 <aswinjayan94@gmail.com>
2021-12-23 13:00:09 +0530

    Output two paths

--- tasks/districts/FetchMumbaiWardsTask.py
+++ tasks/districts/FetchMumbaiWardsTask.py
@@ -16,11 +16,11 @@ class FetchMumbaiWardsTask(luigi.Task):
 
         df, df_2 = scrape_mumbai_pdf(self.input().path)
 
-        df.to_csv(self.output().path, index=False)
-        df_2.to_csv('new_' + self.output().path, index=False)
+        df.to_csv(self.output()[0].path, index=False)
+        df_2.to_csv(self.output()[1].path, index=False)
 
     def output(self):
-        return luigi.LocalTarget(f'mumbai_{date.today()}.csv')  # TODO this can fail if the run is very close 23:59
+        return luigi.LocalTarget(f'mumbai_{date.today()}.csv'), luigi.LocalTarget(f'mumbai_overall_{date.today()}.csv')  # TODO this can fail if the run is very close 23:59
 
     def complete(self):
         return self.output().exists()
