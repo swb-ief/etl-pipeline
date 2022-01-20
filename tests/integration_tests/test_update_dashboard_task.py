faf642a08781c449a0460f8e2d865056c4bff58f
aswinjayan94 <aswinjayan94@gmail.com>
2022-01-20 09:28:35 +0530

    Update test_update_dashboard_task.py

--- tests/integration_tests/test_update_dashboard_task.py
+++ tests/integration_tests/test_update_dashboard_task.py
@@ -41,7 +41,7 @@ class TestUpdateDashboardTask(unittest.TestCase):
         ]]
 
         def mock_exists(self, storage_name):
-            if storage_name in ['raw_ward_data', 'raw_district_overview_stats']:
+            if storage_name in ['raw_ward_data', 'raw_district_overview_stats', 'Phase 2 - District Overview']:
                 return False
             return True
 
